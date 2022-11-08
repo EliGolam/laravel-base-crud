ba25dab6cd69ca565de53839ab6e5fa99434e0fe
EliGolam <elistasibusiness@gmail.com>
2022-11-08 21:34:37 +0100

    Imported previous work done on DC Comics

---
+++ config/data/nav-links.php
@@ -0,0 +1,18 @@
+<?php
+
+    return [
+
+        "characters" => "/characters",
+        "comics" => "/comics",
+        "movies" => "/movies",
+        "tv" => "/tv",
+        "games" => "/games",
+        "collectibles" => "/collectibles",
+        "videos" => "/videos",
+        "fans" => "/fans",
+        "news" => "/news",
+        "shop" => "/shop",
+
+    ]
+
+?>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,31 @@
-<header>
-    THIS IS A HEADER
+@php
+    $links = config('data.nav-links');
+@endphp
+
+<header class="container flex ">
+    <h1 class="visually-hidden">DC Comics</h1>
+    <div class="logo">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="DC Comics Logo" class="img-fluid">
+    </div>
+
+    {{-- @dd($links) --}}
+    <nav class="top-nav nav-bar" role="navigation" aria-label="Webpages navigation">
+        <h2 class="visually-hidden">Webpages Navigation</h2>
+
+        <button id="top-nav-open-menu" class="btn nav-toggle"><i class="fa-solid fa-bars"></i></button>
+
+        <ul class="flex nav-list">
+            @foreach ($links as $pageName => $link)
+
+                <li class="nav-link {{ request()->is($pageName) ? 'active' : '' }}"><a href="{{ $link }}">
+                    {{ $pageName }}
+                </a></li>
+            @endforeach
+        </ul>
+    </nav>
+
+
+    <form>
+        <input type="text" class="search-bar" placeholder="search" />
+    </form>
 </header>
